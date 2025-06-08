4c9b7bc16896c8b0858d1d19bf1a218aa962ba68
0riL <ipfyekly@gmail.com>
2025-06-09 03:32:36 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,6 @@
-
+// Basic Express server skeleton
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+app.get('/', (req, res) => res.send('Roblox Username Hunter Bot backend running!'));
+app.listen(port, () => console.log(`Server listening on port ${port}`));
