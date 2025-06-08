1d7df48ce5635723bebc111853fd72bbc3004c74
0riL <ipfyekly@gmail.com>
2025-06-09 03:34:39 +0530

    Update errorDiagnostics.js

--- backend/bot/errorDiagnostics.js
+++ backend/bot/errorDiagnostics.js
@@ -1,1 +1,6 @@
-
+// Placeholder for error reporting
+function reportError(error) {
+  // TODO: Report errors to UI/log
+  console.error('Reported error:', error);
+}
+module.exports = { reportError };
