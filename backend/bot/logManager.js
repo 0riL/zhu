db82e942024b607bba66e3a4a4b44479668632c4
0riL <ipfyekly@gmail.com>
2025-06-09 03:33:57 +0530

    Update logManager.js

--- backend/bot/logManager.js
+++ backend/bot/logManager.js
@@ -1,1 +1,6 @@
-
+// Placeholder for logging logic
+function logCheck(username, result) {
+  // TODO: Save to persistent storage
+  console.log('Checked:', username, result);
+}
+module.exports = { logCheck };
