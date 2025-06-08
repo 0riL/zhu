217bbaddb8bc7bb2d17f65ef8271f278cf102b39
0riL <ipfyekly@gmail.com>
2025-06-09 03:33:07 +0530

    Update usernameChecker.js

--- backend/bot/usernameChecker.js
+++ backend/bot/usernameChecker.js
@@ -1,1 +1,6 @@
-
+// Placeholder for username checking logic
+async function checkUsername(username) {
+  // TODO: Add Roblox API call
+  return { username, available: null };
+}
+module.exports = { checkUsername };
